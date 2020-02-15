d2096c9ed92d19dbc32c6b5167eb2a9c260a7298
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-14 23:46:53 -0500

    added the initial questions to the app

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Manager = require('./lib/manager.js');
-const Employee = require('./lib/employee.js');
-const Intern = require('./lib/intern.js');
+const Manager = require('./lib/Manager.js');
+const Employee = require('./lib/Employee.js');
+const Intern = require('./lib/Intern.js');
+const Engineer = require('./lib/Engineer.js');
+
+employeesinfo = () => {
+	inquirer
+		.prompt([
+			{
+				type    : 'list',
+				name    : 'employeerole',
+				message : 'What is your Role in this Great Company?',
+				choices : [
+					{
+						name  : 'Manager',
+						value : new Manager()
+					},
+					{
+						name  : 'Engineer',
+						value : new Engineer()
+					},
+					{
+						name  : 'Intern',
+						value : new Intern()
+					}
+				]
+			}
+		])
+		.then((data) => {
+			const employeetype = data.employeerole;
+			console.log(employeetype);
+		});
+};
+employeesinfo();
