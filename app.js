7ea2b7525535b195e1e100746fec8f6c3c7f239c
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-14 20:37:44 -0500

    updated the initial code for the app

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require('./lib/manager.js');
+const Employee = require('./lib/employee.js');
+const Intern = require('./lib/intern.js');
+
+const team = new Employee();
+
+team.employeesinfo();
