093d6e446246f4e7df6aeeb5c9dd65878c92b479
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-21 19:09:43 -0500

    try to get the questions function working, making progress...

--- app.js
+++ app.js
@@ -5,48 +5,46 @@ const Employee = require('./lib/Employee.js');
 const Intern = require('./lib/Intern.js');
 const Engineer = require('./lib/Engineer.js');
 
-employeesinfo = () => {
-	inquirer
-		.prompt([
-			{
-				type    : 'list',
-				name    : 'employeerole',
-				message : 'What is your Role in this Great Company?',
-				choices : [
-					{
-						name  : 'Manager',
-						value : new Manager()
-					},
-					{
-						name  : 'Engineer',
-						value : new Engineer()
-					},
-					{
-						name  : 'Intern',
-						value : new Intern()
-					}
-				]
-			}
-		])
-		.then((data) => {
-			const employeetype = data.employeerole;
-			employeetype.inquirer.prompt([
+const getRole = function() {
+	inquirer.prompt([
+		{
+			type         : 'list',
+			memberChoice : 'employeerole',
+			message      : 'What is your Role in this Great Company?',
+			choices      : [
 				{
-					type    : 'input',
-					name    : 'employeename',
-					message : 'What is your name?'
+					name  : 'Manager',
+					value : new Manager()
 				},
 				{
-					type    : 'input',
-					name    : 'id',
-					message : 'Whats your ID#?'
+					name  : 'Engineer',
+					value : new Engineer()
 				},
 				{
-					type    : 'input',
-					name    : 'employeetitle',
-					message : 'What is your title?'
+					name  : 'Intern',
+					value : new Intern()
 				}
-			]);
-		});
-};
-employeesinfo();
+			]
+		}
+	]);
+}.then((userChoice) => {
+	switch (userChoice.memberChoice) {
+		case 'Manager':
+			addEngineer();
+			{
+				inquirer.prompt([
+					{
+						type    : 'input',
+						name    : 'github',
+						message : 'What is your GitHub Account?'
+					}
+				]);
+			}
+			break;
+		default:
+			buildTeam();
+	}
+});
+
+getRole();
+console.log(getRole);
